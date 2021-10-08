10c96c873c1e19bf95236e507c0dd90592f17f89
abhiaswale <aswaleabhishek8@gmail.com>
2021-10-08 15:58:50 +0530

    responsiveness

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -4,6 +4,10 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const numberOfTasks = useSelector((state) => state.tasks.taskCount);
 
-  return <div className={classes.count}>Tasks Remaining : {numberOfTasks}</div>;
+  return (
+    <div className={classes.count}>
+      <div>Tasks Remaining : {numberOfTasks}</div>
+    </div>
+  );
 };
 export default Counter;
